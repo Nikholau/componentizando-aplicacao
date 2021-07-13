9cde58ee7480c2933ab9f619b04f734ab0b98097
Nikholau <nikholasyan12@gmail.com>
2021-07-13 14:36:55 -0300

    sidebar_animado_adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
+import { MoviesProvider } from './MoviesContext';
+
 
 import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
-import { MoviesProvider } from './MoviesContext';
 
 export function App() {
   return (
+
     <MoviesProvider>
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar />
       <Content />
     </div>
     </MoviesProvider>
+
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,18 +1,54 @@
+import { useState } from "react";
 import { useMovies } from "../MoviesContext";
 import { Button } from "./Button";
+import { AiOutlineDoubleLeft } from "react-icons/Ai";
+import { AiOutlineDoubleRight } from "react-icons/Ai";
 
 export function SideBar() {
-  const {genres, handleClickButton, selectedGenreId} = useMovies();
+  const { genres, handleClickButton, selectedGenreId } = useMovies();
+
+  const [isHidden, setIsHidden] = useState(false);
+
+  function handleHideSideBar() {
+    if (!isHidden) {
+
+      setIsHidden(true);
+
+    } else {
+
+      setIsHidden(false);
+    }
+  }
 
   return (
-    <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+    <>
+      <nav className={isHidden ? `${'sidebar sidebar-hidden'}` : `${'sidebar'}`}>
+
+        <div className="hide-button-container">{!isHidden
+          ?
+          <AiOutlineDoubleLeft
+            className="botao-esquerda"
+            onClick={handleHideSideBar}
+          />
+          :
+          <AiOutlineDoubleRight className="botao-direita"
+            onClick={handleHideSideBar}
+          />
+        }
+        </div>
+
+        <div>{!isHidden &&
+          <span>Watch<p>Me</p></span>
+        }
+        </div>
+
+
 
         <div className="buttons-container">
           {genres.map(genre => (
             <Button
               id={String(genre.id)}
-              title={genre.title}
+              title={!isHidden ? genre.title : ''}
               iconName={genre.name}
               onClick={() => handleClickButton(genre.id)}
               selected={selectedGenreId === genre.id}
@@ -21,5 +57,6 @@ export function SideBar() {
         </div>
 
       </nav>
+    </>
   )
 }
